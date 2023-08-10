a948432ce59180869a491c534523c71afbb2f8dd
GlimmerJR <jd.ramirezmendoza06@gmail.com>
2023-08-09 22:55:49 -0500

    Cambios solo para tenerlos en la otra PC  :v

--- App-Inspirada-Fronted/src/componentes/Menus/NavBarSuperior.jsx
+++ App-Inspirada-Fronted/src/componentes/Menus/NavBarSuperior.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, {  useState } from 'react';
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import './styleNavBarSuperior.css'
 
 export const NavBarSuperior = () => {
+
+  const [activeKey, setActiveKey] = useState('home');
+
   return (
     <Container>
-      <Nav fill variant="tabs" defaultActiveKey="/home">
+      <Nav fill variant="tabs" activeKey={activeKey} onSelect={(key) => setActiveKey(key)}>
         <Nav.Item>
-          <Nav.Link href="/home">Inicio</Nav.Link>
+          <Nav.Link className={activeKey === 'home' ? 'active-tab' : ''} eventKey="home" href="/home">Inicio</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="/ovpgs" href='/ovpgs'>OVPGS</Nav.Link>
+          <Nav.Link className={activeKey === 'ovpgs' ? 'active-tab' : ''} eventKey="ovpgs" href='/ovpgs'>OVPGS</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>IGIrl</Nav.Link>
+          <Nav.Link eventKey="igirl" disabled>IGirl</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>Open Day</Nav.Link>
+          <Nav.Link eventKey="openday" disabled>Open Day</Nav.Link>
         </Nav.Item>
       </Nav>
     </Container>
