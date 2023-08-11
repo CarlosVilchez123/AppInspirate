6efeff297140c1203830b41caaa1abde32af8c66
CarlosVilchez123 <carloslvt123@gmail.com>
2023-08-10 21:20:56 -0500

    se implemento la logica de checkbox

--- App-Inspirada-Fronted/src/pages/Ovpgs/Ovpgs.jsx
+++ App-Inspirada-Fronted/src/pages/Ovpgs/Ovpgs.jsx
@@ -2,22 +2,27 @@ import React, { useState } from 'react'
 import { Row, Col} from 'react-bootstrap'
 import { NavBarSuperior} from '../../componentes/Menus/NavBarSuperior'
 import { FormGroup, FormControlLabel, Checkbox} from '@mui/material'
-
+import listaCarreras from './ListaCarreras'
 import './styleOvpgs.css'
 import './styleCheckBox.css'
-const Items = ({nombre}) =>{
+const Items = ({nombre, checked, onChange}) =>{
   return (
     <div className='Item-Personalitation'>
-      <CheckboxPersonalitation label={nombre}/>
+      <CheckboxPersonalitation label={nombre} checked={checked} onChange={onChange}/>
     </div>
   )
 }
 
-const CheckboxPersonalitation = ({label}) =>{
+const CheckboxPersonalitation = ({label, checked, onChange}) =>{
   return (
     <div className='container-check'>
       <label className='checkbox-label'>
-        <input className='input-checkbox' type='checkbox'/>
+        <input 
+          className='input-checkbox' 
+          type='checkbox'
+          checked={checked}
+          onChange={onChange}
+        />
         <span className='span-checkbox'></span>
       </label>
       <p className='p-check'>{label}</p>
@@ -28,29 +33,35 @@ const CheckboxPersonalitation = ({label}) =>{
 
 export const Ovpgs = () => {
 
-  const listaCarreras =[
-    {key: "FC", nombre: "Facultad de Ciencas" , carreras: ["CC", "Quimica", "Fisica","Ing. Fisica", "Matematica"]},
-    {key: "FAUA", nombre: "Facultad de Arquitectura, Urbanismo y Artes" , carreras: ["CC", "Quimica", "Fisica","Ing. Fisica", "Matematica"]},
-    {key: "FIA", nombre: "Facultad de Ingeniería Ambiental" , carreras: ["CC", "Quimica", "Fisica","Ing. Fisica", "Matematica"]},
-    {key: "FIC", nombre: "Facultad de Ingeniería Civil" , carreras: ["CC", "Quimica", "Fisica","Ing. Fisica", "Matematica"]},
-    {key: "FIEECS", nombre: "Facultad de Ingeniería Económica, Estadística y Ciencias Sociales" , carreras: ["CC", "Quimica", "Fisica","Ing. Fisica", "Matematica"]},
-    {key: "FIEE", nombre: "Facultad de Ingeniería Eléctrica y Electrónica" , carreras: ["CC", "Quimica", "Fisica","Ing. Fisica", "Matematica"]},
-    {key: "FIGMM", nombre: "Facultad de Ingeniería Geológica, Minera y Metalúrgica" , carreras: ["CC", "Quimica", "Fisica","Ing. Fisica", "Matematica"]},
-    {key: "FIIS", nombre: "Facultad de Ingeniería Industrial y De Sistemas" , carreras: ["CC", "Quimica", "Fisica","Ing. Fisica", "Matematica"]},
-    {key: "FIM", nombre: "Facultad de Ingeniería Mecánica" , carreras: ["CC", "Quimica", "Fisica","Ing. Fisica", "Matematica"]},
-    {key: "FIQT", nombre: "Facultad de Ingeniería Química y Textil" , carreras: ["CC", "Quimica", "Fisica","Ing. Fisica", "Matematica"]},
-  ]
-  
   const [arreglo, setArreglo] = useState(listaCarreras)
+  
+  const handleCheckboxChange = (event,{index}) =>{
+    console.log(event.target.checked, " ", index)
+    const updateArreglo = [...arreglo]
+    if(event.target.checked===true){
+      updateArreglo[index].checked=true
+      for(let i=0; i<updateArreglo.length; i++){
+        if(i!=index) updateArreglo[i].checked=false;
+      }
+    }
+    console.log(updateArreglo)
+    setArreglo(updateArreglo)
+  }
 
   return (
     <>
         <NavBarSuperior/>
 
         <h1>Carreras</h1>
-        <div>
-          {arreglo.map(item => <Items key={item.key} nombre = {item.nombre}></Items>)}
-        </div>
+        {arreglo.map(
+          (items, index) =>
+          <Items
+            key={items.id}
+            nombre={items.nombre}
+            checked={items.checked}
+            onChange={(event) => handleCheckboxChange(event, { index })}
+          />
+        )}
     </>
   )
 }
