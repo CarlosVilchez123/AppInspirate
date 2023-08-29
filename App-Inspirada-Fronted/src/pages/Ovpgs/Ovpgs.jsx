340090acb6f2cb3f39bb01fd6dd9540202bcb5bd
CarlosVilchez123 <carloslvt123@gmail.com>
2023-08-29 00:27:12 -0500

    se modifico la rama carlos

--- App-Inspirada-Fronted/src/pages/Ovpgs/Ovpgs.jsx
+++ App-Inspirada-Fronted/src/pages/Ovpgs/Ovpgs.jsx
@@ -1,4 +1,101 @@
-import React, { memo, useState } from 'react'
+import React, { useState, useEffect} from 'react'
+import listaFacultades from './Listas/ListaFacultades'
+import CheckBoxIuni from '../../Personalizados/CheckBoxIuni'
+import './styleOvpgs.css'
+import { NavItem } from 'react-bootstrap'
+
+export const Ovpgs = () => {
+  let OvpgUser=''
+  let carrera=''
+  let embajador=''
+  let typesOvpg = ['Grupal', 'Individual']
+  let CheckTypeOvpg = [false, false];
+
+  let [arrayFacultades, setArrayFacultades] = useState(listaFacultades)
+
+  //Hooks
+  //hook del array de los tipos de OVPG
+  let [isTypeOpvg, setTypeOvpg] = useState(typesOvpg) 
+  //hook para el array de checking de los tipos de Ovpg
+  let [isCheckTypeOvpg, setCheckTypeOvpg] = useState(CheckTypeOvpg)
+  //hook para los usarios de Ovpgs
+  let [isOvpgUser, setOvpgUser] = useState(OvpgUser)
+
+  const handleFacultades = (event,{index}) => {
+    const updateArreglo = [...arrayFacultades]
+    if(event.target.checked){
+      updateArreglo[index].checked=true
+      for(let i=0; i<updateArreglo.length; i++){
+        if(i!==index) updateArreglo[i].checked=false;
+      }
+    }else{
+      updateArreglo[index].checked=false;
+    }
+    setArrayFacultades(updateArreglo)
+  }
+
+  const handleTypesOvpgs = (event,{index}) =>{
+    const updateArreglo = [...isCheckTypeOvpg]
+    if(event.target.checked){
+      setOvpgUser(isTypeOpvg[index])
+      updateArreglo[index]=true
+      for(let i=0; i<updateArreglo.length; i++){
+        if(i!==index) updateArreglo[i]=false;
+      }
+    }else{
+      updateArreglo[index]=false;
+    }
+    
+    setCheckTypeOvpg(updateArreglo)
+  }
+
+  useEffect(() => {
+   
+  }, [isOvpgUser]);
+
+  return (
+    <>
+      <div className='typesOvpgs-container'>
+        {isTypeOpvg.map((tipoOvpg,index) => (
+          <CheckBoxIuni 
+            key={index} 
+            label={tipoOvpg} 
+            checked={isCheckTypeOvpg[index]} 
+            onChange={(event) => {handleTypesOvpgs(event,{index})} }
+          
+          />
+        ))}
+      </div>
+
+      <div>
+        {arrayFacultades.map((facus, index) =>(
+          <div key={index} className='facus'>
+            <CheckBoxIuni 
+              label={facus.nombre} 
+              checked={facus.checked} 
+              onChange={(event) => {handleFacultades(event,{index})} }
+            />
+            {facus.checked && 
+              facus.carreras.lista.map(
+                (carrera,indexSon) =>(
+                  <div key={indexSon} className='carreras'>
+                    <CheckBoxIuni 
+                      label={carrera}
+                    />
+                  </div>
+                )
+              )
+            }
+          </div>
+            
+        )
+        )}
+      </div>
+    </>
+  )
+}
+
+/*
 import { NavBarSuperior} from '../../componentes/Menus/NavBarSuperior'
 import listaCarreras from './ListaCarreras'
 import './styleCheckBox.css'
@@ -10,36 +107,7 @@ const Items = ({nombre, lista, checked, onChange}) =>{
   )
 }
 
-const CheckboxPersonalitation = ({label, lista, checked, onChange}) =>{
-  return (
-    <>
-    <div className='container-check'>
-      <label className='checkbox-label'>
-        <input 
-          className='input-checkbox' 
-          type='checkbox'
-          checked={checked}
-          onChange={onChange}
-        />
-        <span className='span-checkbox'></span>
-      </label>
-      <p className='p-check'>{label}</p>
-    </div>
 
-    <div className='container-carreras'>
-      {checked && lista.map((items, i) => (
-              <React.Fragment key={i}>
-                <Items
-                  nombre={items}
-                />
-              </React.Fragment>
-            ))
-      }
-    </div>
-    
-    </>
-  )
-}
 
 export const Ovpgs = () => {
   const [arreglo, setArreglo] = useState(listaCarreras)
@@ -77,3 +145,4 @@ export const Ovpgs = () => {
     </>
   )
 }
+*/
